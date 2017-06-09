25723d64517d9fbaa20ff6b380710699b687800c
taktod <poepoemix@hotmail.com>
2017-06-09 22:00:00 +0900

    buildしわすれてた。やりにくいな・・・このあたり

--- node/index.js
+++ node/index.js
@@ -20,7 +20,7 @@ var Rtmp = (function () {
         this.audioCodecs = [];
         this.videoCodecs = [];
         // 1.codecListの配列に追加したいものを書いておく
-        this.codecList = ["MacOS", "Windows", "Faac", "Openh264", "X264"];
+        this.codecList = ["MacOS", "Windows", "Faac", "Openh264", "X264"]; // windowsはあとで作っておく
     }
     // 2.追加した名前と同じ名称の関数を準備する
     Rtmp.prototype.MacOS = function (target) {
@@ -75,41 +75,22 @@ var Rtmp = (function () {
     };
     Rtmp.prototype.Windows = function (target) {
         if (!target) {
-            if (ttg.encoder.MSAacEncoder.enabled) {
-                this.audioCodecs.push({
-                    codec: "Windows",
-                    name: "Windows",
-                    sampleRate: 44100,
-                    channelNum: 1,
-                    bitrate: { type: "number", value: 96000, values: 96000 },
-                });
-            }
-            if (ttg.encoder.MSH264Encoder.enabled) {
-                // これは本来なら、codecの名前でそれぞれのをつくっておく
-                this.videoCodecs.push({
-                    codec: "Windows",
-                    name: "Windows",
-                    type: { type: "select", value: "H264 Encoder MDT", values: ["H264 Encoder MDT"] },
-                    width: 320,
-                    height: 240,
-                    semiPlanar: true,
-                    bitrate: { type: "number", value: 600000, values: 600000 }
-                });
-            }
         }
         else {
         }
     };
     Rtmp.prototype.Faac = function (target) {
         if (!target) {
-            this.audioCodecs.push({
-                codec: "Faac",
-                name: "Faac",
-                type: "low",
-                bitrate: { type: "number", value: 96000, values: 96000 },
-                sampleRate: 44100,
-                channelNum: 1
-            });
+            if (ttg.encoder.FaacEncoder.enabled) {
+                this.audioCodecs.push({
+                    codec: "Faac",
+                    name: "Faac",
+                    type: "low",
+                    bitrate: { type: "number", value: 96000, values: 96000 },
+                    sampleRate: 44100,
+                    channelNum: 1
+                });
+            }
         }
         else {
             this.audioCodec = target;
@@ -176,82 +157,8 @@ var Rtmp = (function () {
         });
         // 初期設定つくっておく必要がある。
         electron_1.ipcMain.on(this.name + "init", function (event, args) {
-            /*      // 初期化したときの動作
-                  // こっちから利用可能なコーデックを送信しておく。
-                  var audioCodecs:any[] = [];
-                  if(ttg.encoder.AudioConverterEncoder.enabled) {
-                    audioCodecs.push({
-                      name: "MacOS",
-                      type: "aac",
-                      bitrate: {type: "number", value: 96000, values:96000},
-                      channelNum: 1,
-                      sampleRate: 44100
-                    });
-                    // 設定に必要なものもいれておくか・・・その方がやりやすそう
-                  }
-                  if(ttg.encoder.FaacEncoder.enabled) {
-                    audioCodecs.push({
-                      name: "Faac",
-                      type: "low",
-                      bitrate: {type: "number", value: 96000, values:96000},
-                      sampleRate: 44100,
-                      channelNum: 1
-                    });
-                  }
-                  if(ttg.encoder.MSAacEncoder.enabled) {
-                    audioCodecs.push({
-                      name: "Windows",
-                      sampleRate: 44100,
-                      channelNum: 1,
-                      bitrate: {type: "number", value: 96000, values:96000},
-                    });
-                  }
-                  var videoCodecs:any[] = [];
-                  if(ttg.encoder.MSH264Encoder.enabled) {
-                    // これは本来なら、codecの名前でそれぞれのをつくっておく
-                    videoCodecs.push({
-                      name: "Windows",
-                      type: {type: "select", value:"H264 Encoder MDT", values:["H264 Encoder MDT"]},
-                      width: 320,
-                      height: 240,
-                      semiPlanar: true,
-                      bitrate: {type: "number", value: 600000, values: 600000}
-                    });
-                  }
-                  if(ttg.encoder.Openh264Encoder.enabled) {
-                    videoCodecs.push({
-                      name: "Openh264",
-                      width: 320,
-                      height: 240,
-                      param: {type: "textarea", value: "", values: ""},
-                      semiPlanar: false,
-                      spatialParamArray: {type: "textarea", value: "", values: ""}
-                    });
-                  }
-                  if(ttg.encoder.VtCompressSessionEncoder.enabled) {
-                    videoCodecs.push({
-                      name: "MacOS",
-                      width: 320,
-                      height: 240,
-                      fps: 15,
-                      bitrate: {type: "number", value: 600000, values: 600000},
-                      semiPlanar: false,
-                      isbaseline: {type: "checkbox", value: 1, values: 1}
-                    });
-                  }
-                  if(ttg.encoder.X264Encoder.enabled) {
-                    videoCodecs.push({
-                      name: "X264",
-                      width: 320,
-                      height: 240,
-                      preset: {type: "select", value: "veryfast", values: ["ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow", "placebo"]},
-                      tune: {type: "select", value: "zerolatency", values: ["film", "animation", "grain", "stillimage", "psnr", "ssim", "fastdecode", "zerolatency"]},
-                      profile: {type: "select", value: "baseline", values: ["baseline", "main", "high"]},
-            //          profile: {type: "text", value: "baseline", values: ["baseline", "main", "high", "high10", "high422", "high444"]},
-                      semiPlanar: false,
-                      params: {type: "textarea", value: "", values: ""}
-                    });
-                  }*/
+            // 初期化したときの動作
+            // encoderの情報を送っておく
             event.sender.send(_this.name + "init", {
                 audio: _this.audioCodecs,
                 video: _this.videoCodecs
@@ -337,72 +244,6 @@ var Rtmp = (function () {
                     if (event.info.code == "NetStream.Publish.Start") {
                         _this[args.audio.codec](args.audio);
                         _this[args.video.codec](args.video);
-                        //            this.audioCodec = args.audio;
-                        /*            // videoとaudioのencoderを作っておく必要がありそう。
-                                    switch(this.audioCodec.name) {
-                                    case "MacOS":
-                                      this.aacEncoder = new ttg.encoder.AudioConverterEncoder(
-                                        "aac",
-                                        this.audioCodec.sampleRate,
-                                        this.audioCodec.channelNum,
-                                        parseInt(this.audioCodec.bitrate));
-                                      break;
-                                    case "Faac":
-                                      this.aacEncoder = new ttg.encoder.FaacEncoder(
-                                        this.audioCodec.type,
-                                        this.audioCodec.sampleRate,
-                                        this.audioCodec.channelNum,
-                                        this.audioCodec.bitrate,
-                                      );
-                                      break;
-                                    case "Windows":
-                                      break;
-                                    default:
-                                      break;
-                                    }
-                                    this.videoCodec = args.video;
-                                    if(this.videoCodec.semiPlanar) {
-                                      this.subType = "semiPlanar";
-                                      this.uvStride = this.videoCodec.width;
-                                    }
-                                    else {
-                                      this.subType = "planar";
-                                      this.uvStride = this.videoCodec.width / 2;
-                                    }
-                                    switch(this.videoCodec.name) {
-                                    case "MacOS":
-                                      this.h264Encoder = new ttg.encoder.VtCompressSessionEncoder(
-                                        "h264",
-                                        this.videoCodec.width,
-                                        this.videoCodec.height,
-                                        this.videoCodec.fps,
-                                        this.videoCodec.bitrate,
-                                        this.videoCodec.isbaseline);
-                                      break;
-                                    case "Openh264":
-                                      this.h264Encoder = new ttg.encoder.Openh264Encoder(
-                                        this.videoCodec.width,
-                                        this.videoCodec.height,
-                                        {},
-                                        [{}]);
-                                      break;
-                                    case "X264":
-                                      this.h264Encoder = new ttg.encoder.X264Encoder(
-                                        this.videoCodec.width,
-                                        this.videoCodec.height,
-                                        this.videoCodec.preset,
-                                        this.videoCodec.tune,
-                                        this.videoCodec.profile,
-                                        {}
-                                      );
-                                      break;
-                                    case "Windows":
-                                      // それぞれの動作用のがあるわけだが・・・
-                                      break;
-                                    default:
-                                      break;
-                                    }
-                                    */
                     }
                 });
                 _this.ns.publish(args.streamName);
